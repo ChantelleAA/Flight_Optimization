73c230be890b2d4e5f7a7c610eedd512a398de41
ChantelleAA <chantelle.amoako-atta@aims.ac.rw>
2025-05-29 10:07:01 +0000

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+WORKBOOK        = r"C:\path\FlightWorkbook.xlsx"
+DEPARTURE_DATES = ["2025-07-06", "2025-07-07"]
+RETURN_DATE     = "2025-07-09"
+CURRENCY        = "USD"
+
+CITY_COL        = "City of Airport"
+HEADCOUNT_COL   = "participant count"
+
+WAIT_PRICE      = 25      # seconds
+LOG_FILE        = "scraper.log"
